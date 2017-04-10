8d7621f51ddf1638a163a5176ec6bd9846101683
majie <mj461657053@sina.com>
2017-04-10 14:16:17 +0800

    Initial commit

---
+++ src/main/java/Application.java
@@ -0,0 +1,12 @@
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Administrator on 2017/4/10 0010.
+ */
+
+@SpringBootApplication
+public class Application {
+public static void main(String[] args){
+
+}
+}
